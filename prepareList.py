ddf9a3cbc1869a93cfbb800a9c0e98e2aad61843
Colin Torney <colin.j.torney@gmail.com>
2017-12-20 16:57:53 +0000

    save author ids

--- prepareList.py
+++ prepareList.py
@@ -23,10 +23,9 @@ df = pd.read_csv('author_list.txt',names=['lastname','firstname'])
 
 
 
-
-
 for index, author in df.iterrows():
-    
+    if index<46:
+        continue
     ## Initialize author search object and execute search
     auth_srch = ElsSearch('authlast('+author.lastname+')+AUTHFIRST('+author.firstname+')','author')
     auth_srch.execute(client)
@@ -37,24 +36,49 @@ for index, author in df.iterrows():
         print(author.lastname, author.firstname, 'not found')
         continue
     
-       
-    aid = auth_srch.results[0]['dc:identifier']
-    aid=aid[-11:]
+    
+    if lc==1:
+        try: 
+            aid = auth_srch.results[0]['dc:identifier']
+            aid=aid[-11:]
+            lst.append([author.lastname, author.firstname, index, aid])
+        except:
+            print(author.lastname, author.firstname, 'no ID')
+        
 
-    df.set_value(index,'ID',aid)
+    
     
     if lc>1:
         print ("search returned", lc, "results.")
         for i in range(lc):
             
             print('====== option ' + str(i) +' ==========')
-            aid = auth_srch.results[i]['dc:identifier']
-            aid=aid[-11:]
+            try:
+                aid = auth_srch.results[i]['dc:identifier']
+                aid=aid[-11:]
+            except:
+                print('no ID for this entry')
+                continue
+                
+            try:
+                print(auth_srch.results[i]['preferred-name'])
+            except:
+                print('no name for this entry')
+                continue
+            try:
+                print(auth_srch.results[i]['affiliation-current'])
+            except:
+                continue
+            user_entry = input("Correct entry? n: ")
+            if user_entry!='n':
+                lst.append([author.lastname, author.firstname, index, aid])
+            
+                
+        print('**********************************************************')
+        print('end of author',author.lastname)
+        print('**********************************************************')
 
-            print(auth_srch.results[i]['preferred-name'])
-            print(auth_srch.results[i]['affiliation-current'])
-            print('author ID:',aid)
-
-    
-df.to_csv('author_ids.csv')
+outDF = pd.DataFrame(lst, columns=['lastname','firstname','uid','aid'])
+ 
+outDF.to_csv('author_ids.csv')
 
